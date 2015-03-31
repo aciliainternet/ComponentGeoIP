c81fb6db77b578b196f294ee739eea70357cf18e
Andrés Montañez <andresmontanez@gmail.com>
2015-03-31 14:35:31 -0300

    PSR2 Standard

--- Service/GeoIPService.php
+++ Service/GeoIPService.php
@@ -5,38 +5,38 @@ use Exception;
 
 class GeoIPService
 {
-	private $cache;
+    private $cache;
 
-	public function __construct($debug = false)
-	{
-		$this->cache = [];
+    public function __construct($debug = false)
+    {
+        $this->cache = [];
 
-		if (!function_exists('geoip_country_code_by_name')) {
+        if (!function_exists('geoip_country_code_by_name')) {
             if ($debug == true) {
-            	throw new Exception('Function "geoip_country_code_by_name" is not defined. Please verify that PHP GeoIP Module is enabled!');
-
+                throw new Exception('Function "geoip_country_code_by_name" is not defined. Please verify that PHP GeoIP Module is enabled!');
             } else {
-            	// O1 is the code for Other Country -- http://dev.maxmind.com/geoip/legacy/codes/iso3166/
-                function geoip_country_code_by_name($hostname) {
+                // O1 is the code for Other Country -- http://dev.maxmind.com/geoip/legacy/codes/iso3166/
+                function geoip_country_code_by_name($hostname)
+                {
                     return 'O1';
                 }
             }
-		}
-	}
+        }
+    }
 
     public function getCountryCode($ipAddress)
     {
-    	$ipAddressHash = sha1($ipAddress);
+        $ipAddressHash = sha1($ipAddress);
 
-    	if (!isset($this->cache[$ipAddressHash])) {
-    		$countryCode = @geoip_country_code_by_name($ipAddress);
-    		if ($countryCode === false) {
-    			$countryCode = 'O1';
-    		}
+        if (!isset($this->cache[$ipAddressHash])) {
+            $countryCode = @geoip_country_code_by_name($ipAddress);
+            if ($countryCode === false) {
+                $countryCode = 'O1';
+            }
 
-    		$this->cache[$ipAddressHash] = strtoupper($countryCode);
-    	}
+            $this->cache[$ipAddressHash] = strtoupper($countryCode);
+        }
 
-    	return $this->cache[$ipAddressHash];
+        return $this->cache[$ipAddressHash];
     }
 }
